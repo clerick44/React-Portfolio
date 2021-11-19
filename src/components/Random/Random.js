aa34fd9e3ef9caea59cf74c28f13ffa90dbe0465
Jesse Erickson <clerick44@hotmail.com>
2021-11-19 15:32:23 -0600

    footer completed

---
+++ src/components/Random/Random.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Random.css";
+
+const Random = () => {
+  return <h1> Random page placeholder</h1>;
+};
+
+export default Random;
---
+++ src/components/Resume/Resume.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Resume.css";
+
+const Resume = () => {
+  return <h1> Resume page placeholder</h1>;
+};
+
+export default Resume;
