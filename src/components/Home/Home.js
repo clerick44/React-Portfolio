ae68d5d5a05abc67ab6f12ae672aec2ffc093f91
Jesse Erickson <clerick44@hotmail.com>
2022-04-07 19:44:21 -0500

    home layout working and responsive

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,12 +5,15 @@ const Home = () => {
   return (
     <div id="container">
     <div id="hero">
+      <div id="colorSplash"></div>
       <img src="jesse-profile.jpg"></img>
+     <div id="text">
       <h1>
         JESSE ERICKSON
         <span>WEB DEVELOPER</span>
       </h1>
       <p>Full blooded Minnesotan who loves coding and building web aplications. Driven by quality and a need for personal growth I'm passionate about what I do and how I do it and I love sharing that with those around me.</p>
+    </div>
     </div>
     </div>
     
